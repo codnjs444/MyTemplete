77df45dfb2eb812f87630cb627a9681fea434038
jeff131 <user@192.168.3.125>
2024-06-27 10:20:21 +0900

    first

---
+++ MyTemplete/src/main/java/com/example/MyTemplete/MyTempleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyTemplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTempleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTempleteApplication.class, args);
+	}
+
+}
