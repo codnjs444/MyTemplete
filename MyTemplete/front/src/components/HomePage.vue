89d7e44714eb509e504975f7dfa112ffce42fe2d
jeff131 <user@192.168.3.125>
2024-06-28 18:05:08 +0900

    front

--- MyTemplete/front/src/components/HomePage.vue
+++ MyTemplete/front/src/components/HomePage.vue
@@ -1,16 +1,12 @@
 <template>
   <v-app>
     <v-container>
-      <v-row>
-        <v-col cols="12" md="8" offset-md="2">
+      <v-row justify="center">
+        <v-col cols="12">
           <v-card>
-            <v-card-title class="headline">User Information</v-card-title>
-            <v-card-text v-if="user">
-              User ID: {{ user.userId }} <br />
-              User Name: {{ user.userName }}
-            </v-card-text>
-            <v-card-text v-else>
-              Loading...
+            <v-card-title class="headline">홈 페이지</v-card-title>
+            <v-card-text>
+              로그인에 성공하셨습니다! 여기는 홈 페이지입니다.
             </v-card-text>
           </v-card>
         </v-col>
@@ -20,31 +16,10 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-
 export default {
   name: 'HomePage',
-  setup() {
-    const user = ref(null);
-
-    onMounted(() => {
-      axios.get('/user', { params: { userId: 'aaa' } })
-        .then(response => {
-          user.value = response.data;
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    });
-
-    return {
-      user,
-    };
-  },
 };
 </script>
 
 <style scoped>
-/* 필요한 스타일을 추가할 수 있습니다. */
 </style>
--- MyTemplete/front/src/main.js
+++ MyTemplete/front/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 import router from './router';
+import './utils/axios' // axios 설정 파일을 불러옴
 
 loadFonts();
 
